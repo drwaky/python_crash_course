4c1a5cc81f41e8a63b99c0cbfb4f9e2007b02eef
Joaquin Lopez <drwaky@gmail.com>
2024-09-27 17:35:46 -0500

    ✨ Removing prefixes and sufixes

---
+++ prefix_sufix.py
@@ -0,0 +1,7 @@
+URL = "https://www.python.org"
+print(f"Here is the URL: {URL}")
+print(f"Removing prefix 'https://' 👉 {URL.removeprefix('https://')}")
+
+file_name = "hello_world.py"
+print(f"\nFile complete name: {file_name}")
+print(f"Removing sufix '.py' 👉 {file_name.removesuffix('.py')}")
