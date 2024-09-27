7bd042e089d4d5af1c0dc5daaff53327a0dd4954
Joaquin Lopez <drwaky@gmail.com>
2024-09-27 17:59:28 -0500

    ✨ Numbers: Integers

---
+++ integers.py
@@ -0,0 +1,26 @@
+add = 2 + 3
+print(f"Adding 2 + 3: {add}")
+
+subtract = 3 - 2
+print(f"Subtracting 3 - 2: {subtract}")
+
+multiply = 2 * 3
+print(f"Multiplying 2 * 3: {multiply}")
+
+divide = 3 / 2
+print(f"Dividing 3 / 2: {divide}")
+
+exponent = 3 ** 2
+print(f"Exponent 3 ** 2: {exponent}")
+
+exponent2 = 3 ** 3
+print(f"Exponent 3 ** 3: {exponent2}")
+
+exponent3 = 10 ** 6
+print(f"Exponent 10 ** 6: {exponent3}")
+
+operation = 2 + 3 * 4
+print(f"Operation 2 + 3 * 4: {operation}")
+
+operation2 = (2 + 3) * 4
+print(f"Operation (2 + 3) * 4: {operation2}")
