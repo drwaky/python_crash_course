4bab87ca4ac612bb82f2d7b883c11ac72dc48e1b
Joaquin Lopez <drwaky@gmail.com>
2024-09-27 18:53:39 -0500

    ✨ Numbers: Floats

---
+++ floats.py
@@ -0,0 +1,19 @@
+add = 0.1 + 0.1
+print(f"Adding 0.1 + 0.1: {add}")
+
+add2 = 0.2 + 0.2
+print(f"Adding 0.2 + 0.2: {add2}")
+
+multiply = 2 * 0.1
+print(f"Multiplying 2 * 0.1: {multiply}")
+
+multiply2 = 2 * 0.2
+print(f"Multiplying 2 * 0.2: {multiply2}")
+
+divide = 0.2 / 0.1
+print(f"Dividing 0.2 / 0.1: {divide}")
+
+strange = 0.2 + 0.1
+print(f"Strange operation 0.2 + 0.1: {strange}")
+strange2 = 3 * 0.1
+print(f"Strange operation 3 * 0.1: {strange2}")
